a23309ce4be395eacd024760231eecea2c9fd7bc
chiinello <nelojaiko.com>
2021-12-06 14:27:19 +0000

    initial commit

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 
 let uni0=document.getElementById("unicorn0")
 let uni0x=0  //Unicorn0's X position
+let uni1=document.getElementById("unicorn1")
+let uni1x=0
+let uni2=document.getElementById("unicorn2")
+let uni2x=0
+let finish=document.getElementById("finish")
 
 
 //Extra challenges .. Set the background colour of each unicorn .. (so they are different) .. ideally with Javascript (not CSS)
@@ -10,22 +15,50 @@ let uni0x=0  //Unicorn0's X position
 // - What if we wanted 20 unicorns ??  []
 
 
-setInterval(moveUnicorns,100)  //How fast the race is - smaller=faster
+setInterval(moveUnicorns,50)  //How fast the race is - smaller=faster
 
 
 function moveUnicorns(){
 
     uni0.style.left= uni0x +"%"
+    uni0x +=(Math.random()) //add 1% to the unicorns x position (Add a random amount instead)   
+    uni1.style.left= uni1x +"%"
+    uni1x +=(Math.random()) 
+    uni2.style.left= uni2x +"%"
+    uni2x +=(Math.random())
 
-    uni0x +=1  //add 1% to the unicorns x position (Add a random amount instead)
-        
+    
+    
+    
     //Move all unicorns a random amount each
     //If a unicorn has crossed the finish line
+    if(uni0x>90){
+        alert("uni0 wins")
+        uni0x=0
+        uni1x=0
+        uni2x=0
+    }
+     if(uni1x>90){
+        alert("uni1 wins")
+        uni0x=0
+        uni1x=0
+        uni2x=0
+    }
+     if(uni2x>90){
+        alert("uni2 wins")
+        uni0x=0
+        uni1x=0
+        uni2x=0
+    }
+
         //Display winner
         // restart race           
 
 }
 
+// if("uni0"&&"uni1"&&"uni2"){
+//  alert(`hurray $() wins🏆`)
+// }
 
 
 
